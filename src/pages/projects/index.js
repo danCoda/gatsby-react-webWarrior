d81c2542e324349d52a90229308f82c5b914c2f9
Daniel Son <dson.email@gmail.com>
2021-08-06 12:12:31 +1000

    Component added (navbar)

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Navbar from '../components/Navbar'
 
 export default function Projects() {
   return (
     <section>
+      <Navbar />
       <div>
         <h2>Portfolio</h2>
         <h2>Projects & Websites I've created</h2>
